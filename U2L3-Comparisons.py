58ede105beff287d507bc1a6b9d1c0fb93925b6c
Ayaan735229 <735229@pdsb.net>
2024-09-24 09:54:46 -0400

    Optimize U2L3-Comparisons.py
    Reduced the number of loop iterations

--- U2L3-Comparisons.py
+++ U2L3-Comparisons.py
@@ -1,8 +1,8 @@
 arr = ["zebra", "kangaroo", "cat", "human", "aardvark"]
 
 for i in range(len(arr)):
-  for j in range(len(arr)):
-    if arr[i] < arr[j]:
+  for j in range(i, len(arr)):
+    if arr[i] > arr[j]:
       temp = arr[j]
       arr[j] = arr[i]
       arr[i] = temp
