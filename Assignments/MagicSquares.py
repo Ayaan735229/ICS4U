662088f9e23a1efbd66b00cfd484fde06abefb16
Ayaan735229 <735229@pdsb.net>
2024-10-16 20:25:38 -0400

    Added calculateMagicSum() to MagicSquares.py
    Also added comments for each function that didn't have them documenting what each did. Also changed variable names to keep consistency in naming convention.

--- Assignments/MagicSquares.py
+++ Assignments/MagicSquares.py
@@ -26,81 +26,146 @@ def checkValidity(n):
     return True
 
 
-def initRow(dim, zeroed = False):
+def getInitRow(dim, zeroed = False):
     """
     Creates the initial row for the square
     dim (int)         The dimensions of the square
     zeroed (bool)     Determines the type of square
-    arr_init (int[])  First row of of the square
-    """
-    arr_init = []
-    if zeroed:
-        arr_init = [n * dim for n in range(0, dim)]
-    else:
-        arr_init = [n for n in range(1, dim+1)]
-    random.shuffle(arr_init)
-    
-    return arr_init
 
-def generateMagicSquare(dim, shift, zeroed = False):
-    arr_init = initRow(dim, zeroed)
+    initRow (int[])  First row of of the square
+    """
+    initRow = []
+    if zeroed:
+        # for every number, n, from 0 (inclusive) to dim (exclusive), add to the array n * dim
+        initRow = [n * dim for n in range(dim)]
+    else:
+        # for every number, n, from 1 (inclusive) to dim + 1 (exclusive), add to the array n
+        initRow = [n for n in range(1, dim+1)]
+    # shuffle the array
+    random.shuffle(initRow)
+    
+    return initRow
+
+
+def generateTrivialMagicSquare(dim, shift, zeroed = False):
+    """
+    Generates a magic square given the initial row and the shift
+    dim (int)       The dimensions of the square
+    shift (int)     The shift to be used per row
+    zeroed (bool)   Determines the type of square, to be passed to getInitRow
+
+    intRow (int[])  The initial row, supplied by getInitRow
+    M (int[][])     The generated trivial magic square
+    """
+
+    # Initialize the matrix with the first row
+    initRow = getInitRow(dim, zeroed)
     M = []
-    M.append(arr_init)
+    M.append(initRow)
 
     for i in range(dim-1):
+        # Get the latest addition to the Matrix
         tempArr = M[len(M)-1]
         shiftedArr = []
+        # Shift it and add it to the matrix
         for j in range(dim):
             shiftedArr.append(tempArr[(j + dim - shift) % dim])
         M.append(shiftedArr)
     return M
 
+
 def addMatrices(M1, M2):
-    new_M = []
+    """
+    Takes two equal dimension matrices and adds them together
+    M1 (int[][]) The first matrix to add
+    M2 (int[][]) The second matrix to add
+
+    M (int[][]) The summed matrix
+    """
+    M = []
+    # Add values from one matrix to the matching value in the other matrix
     for i in range(len(M1)):
-        temp_row = []
+        tempRow = []
         for j in range(len(M1)):
-            temp_row.append(M1[i][j] + M2[i][j])
-        new_M.append(temp_row)
+            tempRow.append(M1[i][j] + M2[i][j])
+        M.append(tempRow)
         
-    return new_M
+    return M
+
 
 def isMagic(M):
+    """
+    Checks whether a given matrix is a magic square, trivial or normal
+    M (int[][]) The matrix to be checked
+
+    dim (int)           Dimensions of the matrix
+    magicSum (int)      Supposed magic sum of the matrix
+    diagMainSum (int)   Sum of the main diagonal
+    diagSecSum (int)    Sum of the second diagonal
+    currRowSum (int)    Sum of one row
+    currColSum (int)    Sum of one row
+    """
+
     dim = len(M)
+    magicSum = calculateMagicSum(dim)
   
-    # check diagonal
-    diag_main_sum = 0
-    diag_sec_sum = 0
+    # Check diagonal
+    diagMainSum = 0
+    diagSecSum = 0
     for i in range(dim):
-        diag_main_sum += M[i][i]
-        diag_sec_sum += M[i][dim-i-1]
-    if diag_main_sum != diag_sec_sum:
+        diagMainSum += M[i][i]
+        diagSecSum += M[i][dim-i-1]
+    # If the two aren't equal, return early
+    if diagMainSum != diagSecSum != magicSum:
         return False
     
-    # check horizontal/vertical
+    # Check horizontal/vertical
     for i in range(dim):
-        curr_row_sum = 0
-        curr_col_sum = 0
-    for j in range(dim):
-        curr_row_sum += M[i][j]
-        curr_col_sum += M[j][i]
-      
-    if curr_row_sum != curr_col_sum != diag_main_sum:
-        return False
-      
+        currRowSum = 0
+        currColSum = 0
+        for j in range(dim):
+            currRowSum += M[i][j]
+            currColSum += M[j][i]
+
+        # Compare current row, current column, and the magic sum
+        # If the three aren't equal, return early
+        if currRowSum != currColSum != magicSum:
+            return False
+    
+    # If the function has reached this, then the matrix is a magic square
     return True
 
+
+def calculateMagicSum(dim):
+    """
+    Calculates the sum of the Magic Square
+    M (int[][]) The magic square
+
+    magicSumArr (int[]) List of each term to be divided in the magic sum
+    magicSum (int)      The magic sum
+    """
+
+    # Get a list of all the numbers from 1 (inclusive) to dim^2 + 1 (exclusive)
+    magicSumArr = [n for n in range(1, dim**2+1)]
+    magicSum = int(sum(magicSumArr)/dim)
+    return magicSum
+    
+
 if __name__ == "__main__":
+    # Assume that the provided value is NOT valid
     isValid = False
     
     while (not isValid):
-        user_dims = int(input("Enter a prime number from 5-19 (inclusive): "))
-        isValid = checkValidity(user_dims)
+        userDims = int(input("Enter a prime number from 5-19 (inclusive): "))
+        # Update the validity of the inputted dimensions
+        isValid = checkValidity(userDims)
     
-    A = generateMagicSquare(user_dims, 2)
-    B = generateMagicSquare(user_dims, 3, True)
+    # Create both matrices and add them
+    A = generateTrivialMagicSquare(userDims, 2)
+    B = generateTrivialMagicSquare(userDims, 3, True)
     sumMatrix = addMatrices(A, B)
     
+    # Print each matrix, formatted, the magic sum, as well as whether the final matrix is magic
     print("Square One")
     for line in A:
         print(line)
@@ -109,9 +174,11 @@ if __name__ == "__main__":
     for line in B:
         print(line)
     
-    print("\nFinal Magic Square")
+    print("\nMagic Square")
     for line in sumMatrix:
         print(line)
+
+    print("\nMagic Sum:", calculateMagicSum(userDims))
         
-    print(isMagic(sumMatrix))
+    print("This square is Magic:", isMagic(sumMatrix))
 
