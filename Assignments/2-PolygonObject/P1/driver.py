1e05df84737fc3dfdaa654c562f0ba191f098f04
Ayaan735229 <735229@pdsb.net>
2024-12-06 23:06:46 -0500

    Update and rename Assignments/2-PolygonObject/driver.py to Assignments/2-PolygonObject/P1/driver.py
    Moved the file, made minor changes to how files are handled, and shifted some code into a __name__ == "main" block.

--- Assignments/2-PolygonObject/P1/driver.py
+++ Assignments/2-PolygonObject/P1/driver.py
@@ -1,12 +1,11 @@
 from poly import Polygon
 
-def readFile(fileName: str = "a2", fileExt: str = ".txt") -> list[str]:
+def readFile(filePath = "a2.txt") -> list[str]:
     lines = []
-    with open(fileName + fileExt, "r") as fh:
+    with open(filePath, "r") as fh:
         lines = fh.readlines()
     return lines
 
-# TODO: find if the try catch blocks can be replaced
 def convertNumeric(pointStr: str) -> tuple[int|float]:
     xStr, yStr = pointStr.strip("()").split(",")
     try:
@@ -20,20 +19,19 @@ def convertNumeric(pointStr: str) -> tuple[int|float]:
         
     return (x, y)
 
-# TODO: Make this function not so miserable to read
 def getPointsNumeric(pointsStr: str) -> list[tuple[int|float]]:
     pointStrArr = pointsStr.strip().split(", ")
     pointsArr = []
     for pointStr in pointStrArr:
         pointsArr.append(convertNumeric(pointStr))
     return pointsArr
-    
 
-fileText = readFile()
-pointsNumeric = getPointsNumeric(fileText[0])
-p = Polygon()
+if __name__ == "main":
+    fileText = readFile()
+    pointsNumeric = getPointsNumeric(fileText[0])
+    p = Polygon()
 
-for point in pointsNumeric:
-    p.addPoint(point)
+    for point in pointsNumeric:
+        p.addPoint(point)
 
-print(p)
+    print(p)
